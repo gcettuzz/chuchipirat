15077c758ff21ebe75ef0798f85b55ca159a4711
Gio Cettuzzi <gio.cettuzzi@gmail.com>
2024-12-04 22:48:31 +0100

    Einbau von Sentry für Error-Monitoring: #183

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
 
 import App from "../src/components/App/App";
 import {FirebaseContext} from "./components/Firebase/firebaseContext";
+import packageJson from "../package.json";
 
 import "typeface-roboto";
 import "@fontsource/roboto-mono";
@@ -12,15 +14,37 @@ import {CustomDialogContextProvider} from "./components/Shared/customDialogConte
 import {NavigationContextProvider} from "./components/Navigation/navigationContext";
 import Firebase from "./components/Firebase/firebase.class";
 
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  environment: process.env.REACT_APP_ENVIROMENT,
+  release: packageJson.version,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    "localhost",
+    /^https:\/\/chuchipirat\.ch/,
+    /^https:\/\/chuchipirat-tst\.web\.app/,
+  ],
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseContext.Provider value={new Firebase()}>
-      <CustomDialogContextProvider>
-        <NavigationContextProvider>
-          <App />
-        </NavigationContextProvider>
-      </CustomDialogContextProvider>
-    </FirebaseContext.Provider>
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <FirebaseContext.Provider value={new Firebase()}>
+        <CustomDialogContextProvider>
+          <NavigationContextProvider>
+            <App />
+          </NavigationContextProvider>
+        </CustomDialogContextProvider>
+      </FirebaseContext.Provider>
+    </Sentry.ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("root")
 );
